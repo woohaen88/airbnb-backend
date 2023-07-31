d814f6c552d31f7411279b049304e470e3c0a2ff
woohaen88 <woohaen88@gmail.com>
2023-07-31 15:33:54 +0900

    Refactor: House Model 수정
    1. 
    1. Verbose_name 추가
    2. default값 추가 -> True
    3. help_text추가

--- houses/models.py
+++ houses/models.py
@@ -8,7 +8,11 @@ class House(models.Model):
     price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField()
+    pets_allowed = models.BooleanField(
+        verbose_name="Pets Allowed?",
+        default=True,
+        help_text="Does this house allow pets?",
+    )
 
     def __str__(self):
         return self.name
