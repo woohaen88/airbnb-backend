615db10feccad44eaa16e69793b9524701d12e6a
woohaen88 <woohaen88@gmail.com>
2023-07-31 16:40:12 +0900

    Test: UserModel Test
    1. Email로 로그인
    2. Email이 없을 때
    3. superuser Test

---
+++ users/tests/test_user_model.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+def create_user(**params):
+    """create and return a new user"""
+    defaults = {
+        "email": "test@example.com",
+        "password": "testpass123",
+    }
+
+    defaults.update(params)
+    return get_user_model().objects.create_user(**defaults)
+
+
+class UserModelTest(TestCase):
+    def test_create_user_with_email_successful(self):
+        """
+        1. email로 유저 생성
+        2. password validator
+        """
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_email_raises_error(self):
+        """email이 없으면 error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test123!@#")
+
+    # def test_create_user_and_right_values(self):
+    #     user = create_user()
+
+    def test_create_superuser(self):
+        """superuser Test"""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com", "test123!@#"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
