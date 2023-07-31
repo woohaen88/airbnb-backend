e7540b4e915abb7ec86a39ca92ca6c35cb1cc73e
woohaen88 <woohaen88@gmail.com>
2023-07-31 15:40:54 +0900

    Feat: House모델 FK 추가
    House -> User

--- houses/models.py
+++ houses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class House(models.Model):
@@ -15,6 +16,8 @@ class House(models.Model):
         default=True,
         help_text="Does this house allow pets?",
     )
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
