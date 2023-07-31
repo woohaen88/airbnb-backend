c25795fb9f8099bb71f33b7f6377b0982d263845
woohaen88 <woohaen88@gmail.com>
2023-07-31 16:39:25 +0900

    Feat: Email로 로그인 구현
    Username file -> email

--- users/models.py
+++ users/models.py
@@ -1,8 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_field):
+        if not email:
+            raise ValueError("User must have an email address.")
+        name = email.split("@")[0]
+        user = self.model(email=self.normalize_email(email), name=name, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name="email address",
+        unique=True,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
     class GenderChoices(models.TextChoices):
         MALE = ("male", "Male")  # value for database / value for admin page
         FEMALE = "female", "Female"
