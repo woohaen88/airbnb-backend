71cbaef75cf7af7c96bacb7fe511809569b49b3a
woohaen88 <woohaen88@gmail.com>
2023-07-31 15:36:51 +0900

    Refactor: House Model 수정
    1. PositiveIntegerField
        * verbose_name 추가
        * help_text 추가

--- houses/models.py
+++ houses/models.py
@@ -5,7 +5,9 @@ class House(models.Model):
     """Model Definition for House"""
 
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Positive Numbers Only"
+    )
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(
