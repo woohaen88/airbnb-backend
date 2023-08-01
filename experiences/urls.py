62f8cd7d61552e622a69ac299bac004c2f628d9a
woohaen88 <woohaen88@gmail.com>
2023-08-01 17:54:47 +0900

    Feat: Perk REST API 개발
    TDD 완료

---
+++ experiences/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from experiences import views
+
+app_name = "experiences"
+urlpatterns = [
+    path("perks/", views.Perks.as_view(), name="perks-list"),
+    path("perks/<int:perk_id>/", views.PerkDetail.as_view(), name="perk-detail"),
+]
--- experiences/views.py
+++ experiences/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from typing import Any
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from experiences.models import Perk
+from experiences.serializers import PerkSerializer
+
+
+class Perks(APIView):
+    def get(self, request: Request) -> Response:
+        perks = Perk.objects.all()
+        serializer = PerkSerializer(perks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request: Request) -> Response:
+        serializer = PerkSerializer(data=request.data)
+        if serializer.is_valid():
+            perk = serializer.save()
+            return Response(PerkSerializer(perk).data)
+        return Response(serializer.errors)
+
+
+class PerkDetail(APIView):
+    def get_object(self, perk_id: int) -> Any:
+        try:
+            return Perk.objects.get(id=perk_id)
+        except Perk.DoesNotExist:
+            raise NotFound("해당 Perk가 존재하지 않습니다.")
+
+    def get(self, request: Request, perk_id: int) -> Response:
+        perk = self.get_object(perk_id=perk_id)
+        serializer = PerkSerializer(perk)
+        return Response(serializer.data)
+
+    def put(self, request: Request, perk_id: int) -> Response:
+        perk = self.get_object(perk_id)
+        serializer = PerkSerializer(perk, data=request.data)
+        if serializer.is_valid():
+            perk = serializer.save()
+            return Response(PerkSerializer(perk).data)
+        return Response(serializer.errors)
+
+    def delete(self, request: Request, perk_id: int) -> Response:
+        perk = self.get_object(perk_id)
+        return Response(
+            data={"message": "perk delete!!"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
